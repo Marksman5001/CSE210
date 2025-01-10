9d32288d307ac1cd76aee6ebeedc55648983f4b3
Marksman5001 <marksman5001@byui.edu>
2025-01-09 20:11:24 -0700

    sandbox, readme changes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        int x=0;
+        while(x<10){
+            string xstring = x.ToString();
+            Console.Write(xstring);
+            if(x%2 == 0){
+                Console.WriteLine(", X is even!");
+            }
+            else{
+                Console.WriteLine();
+            }
+            x++;
+        }
+
         Console.WriteLine("Hello Sandbox World!");
     }
 }
